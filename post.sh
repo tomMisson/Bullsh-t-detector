b2b82422b7ea67fb09362f83b245ddb76ae0b586
Bumblebee Mountain <BumblebeeMountain@gmail.com>
2019-11-02 19:27:39 +0000

    Add flask api and shell scripts to interact

---
+++ post.sh
@@ -0,0 +1,5 @@
+echo $1
+
+# curl -i -H "Content-Type: application/json" -X POST -d '{"url":"https://www.bbc.co.uk/news/uk-politics-50275383"}' http://localhost:5000/url
+# curl -i -H "Content-Type: application/json" -X POST -d '{"url":"$1"}' http://localhost:5000/url
+curl -i -H "Content-Type: application/json" -X POST -d '{"url":"'$1'"}' http://localhost:5000/url
