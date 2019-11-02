e47daa18cbf6ca4941d6449bdf34f3427c09140e
Tom Misson <11tmisson@gmail.com>
2019-11-02 15:23:35 +0000

    Created basic UI

--- fake-news/src/App.js
+++ fake-news/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Are you getting the full story?</h1>
+        <input type="text" placeholder="URL to news source"/>
+        <br/>
+        <input type="submit"/>
+      </main>
     </div>
   );
 }
