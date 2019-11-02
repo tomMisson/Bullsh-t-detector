ef84f3bbb1cc8a1e3591a400fa69e9358d9aedbc
Tom Misson <11tmisson@gmail.com>
2019-11-02 21:14:56 +0000

    Added result returning

--- fake-news/src/App.js
+++ fake-news/src/App.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import Result from './Result';
 
-function App() {
-  return (
-    <div className="App">
-      <main>
-        <h1>Are you getting the full story?</h1>
-        <input type="text" placeholder="URL to news source"/>
-        <br/>
-        <input type="submit"/>
-      </main>
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    url:""
+  }
+
+  collectURL = (event) =>{
+    this.setState({url:document.getElementById("textBox").value});
+    document.getElementById("textBox").value = "";
+    if(this.state.url!=="")
+    {
+      console.log(this.state.url);
+      //fetch("url/"+this.state.url).then()
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <h1>Are you getting the full story?</h1>
+        
+            <input type="text" id="textBox" placeholder="URL to news source"/>
+            <br/>
+            <input type="submit" onClick={this.collectURL}/>
+        </main>
+        <Result url={this.state.url}/>
+      </div>
+    );
+  }
 }
-
 export default App;
