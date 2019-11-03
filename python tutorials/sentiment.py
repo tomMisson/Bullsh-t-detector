c90e9f321da6df7a2ff4b08819184482706500ff
Tom Misson <11tmisson@gmail.com>
2019-11-03 08:11:58 +0000

    Change sentiment to use  txt

--- python tutorials/sentiment.py
+++ python tutorials/sentiment.py
@@ -11,37 +11,24 @@ from nltk.corpus import names
 def word_feats(words):
     return dict([(word, True) for word in words])
 
-f = open("../dataset.tff", "r")
+f = open("../Dataset.txt", "r")
 
-neededLines = []
 positive=[]
 negative=[]
 neutral=[]
-n=0
 
 for line in f:
     line = line.split(' ')
-   
-
-    if (line[4] == "stemmed1=y" or line[3]=="pos1=noun"):
-        n+=1
-        neededLines.append(line[2]+"="+line[5])
-    
-    else:
-        next
-
-for lines in neededLines:
-    lines = lines.split('=')
-    lines = lines[1] + " " + lines[3].replace("\n", '')
+    line[1] = lines[1].replace("\n", '')
 
     lines = lines.split(" ")
 
     if(lines[1]=="positive"):
-        pos.append(lines[1])
+        positive.append(line[0])
     elif (lines[1] == "neutral"):
-        nut.append(lines[1])
+        neutral.append(lines[0])
     elif (lines[1] == "negative"):
-        neg.append(lines[1])
+        negative.append(lines[0])
 
 
 positive_vocab = positive
